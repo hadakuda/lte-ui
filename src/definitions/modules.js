d54b7be4c0a89001357757fb03c90bfd2e79f337
thj <thj05@126.com>
2023-04-12 19:44:13 +0800

    first commit

---
+++ src/definitions/modules.js
@@ -0,0 +1,13 @@
+export default [{
+    name: "Player",
+    path: "/player"
+}, {
+    name: "Player",
+    path: "/player"
+}, {
+    name: "Test1",
+    path: "/test1"
+}, {
+    name: "Test2",
+    path: "/test2"
+}]
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ViewUIPlus from 'view-ui-plus'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(router)
+    .use(ViewUIPlus)
+    .mount('#app')
